17351f879590e1df9849ac09badc1f7acc3983a4
Sayantanu Dey <41713730+sayantanu-dey@users.noreply.github.com>
2021-01-27 09:50:14 +0530

    Update TheMovieDBInterface.kt

--- app/src/main/java/com/voile/movie/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/voile/movie/data/api/TheMovieDBInterface.kt
@@ -8,8 +8,8 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TheMovieDBInterface {
-    //https://api.themoviedb.org/3/movie/popular?api_key=bad0d83fcc5038e4a9639c9e39782793&language=en-US&page=1
-    //https://api.themoviedb.org/3/movie/497582?api_key=bad0d83fcc5038e4a9639c9e39782793&language=en-US
+    //https://api.themoviedb.org/3/movie/popular?api_key=<API_Key>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/497582?api_key=<API_Key>&language=en-US
     //https://api.themoviedb.org/3/
 
     @GET("movie/popular")
